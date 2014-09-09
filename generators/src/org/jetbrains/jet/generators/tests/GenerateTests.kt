d4e9e844715c1f196726afecbf469aea468d750a
Mikhail Mutcianko <mutcianko.m@gmail.com>
2014-09-09 16:55:34 +0400

    add android goto declaration test stub

--- generators/src/org/jetbrains/jet/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/jet/generators/tests/GenerateTests.kt
@@ -123,6 +123,7 @@ import org.jetbrains.jet.lang.resolve.android.AbstractAndroidXml2KConversionTest
 import org.jetbrains.jet.android.AbstractCrossParserTest
 import org.jetbrains.jet.lang.resolve.android.AbstractAndroidBoxTest
 import org.jetbrains.jet.android.AbstractAndroidCompletionTest
+import org.jetbrains.jet.android.AbstractAndroidGotoTest
 
 fun main(args: Array<String>) {
     System.setProperty("java.awt.headless", "true")
@@ -616,6 +617,10 @@ fun main(args: Array<String>) {
 
         testClass(javaClass<AbstractAndroidCompletionTest>()) {
             model("android/completion", recursive = false, extension = null)
+        }
+
+        testClass(javaClass<AbstractAndroidGotoTest>()) {
+            model("android/goto", recursive = false, extension = null)
         }
     }
 
